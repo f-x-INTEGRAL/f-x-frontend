c315ca561cdd24365a13a25ac19311a8750b3e7d
Gull 2365 <jych2365@gmail.com>
2023-04-16 22:56:41 +0900

    feat: button 컴포넌트 추가

---
+++ src/components/Button/styled.ts
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: left;
+  width: 500px;
+  height: auto;
+  margin-top: 55px;
+`;
+
+export const Button = styled.button`
+  margin: 0;
+  font-size: 35px;
+  color: #ffffff;
+  background-color: #62a1f1;
+  border: none;
+  border-radius: 12px;
+  width: 120px;
+  height: 50px;
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.25);
+  &:hover {
+    box-shadow: inset 0 0px 10px rgba(0, 0, 0, 0.5);
+  }
+`;
+
+export const TextWrapper = styled.div`
+  padding-top: 20px;
+  padding-left: 10px;
+  margin-bottom: 20px;
+  height: auto;
+  margin: 0;
+`;
+
+export const TextBox = styled.text<{ black?: boolean }>`
+  color: ${(props) => (props.black ? '#000' : '#abaaaa')};
+  font-size: 20px;
+`;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './Layout';
 export * from './Navbar';
 export * from './MainTitle';
 export * from './Input';
+export * from './Button';
