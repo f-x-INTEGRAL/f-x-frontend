c252b8b6e263e0585d1df0eff4957c2def72db7e
Gull 2365 <jych2365@gmail.com>
2023-04-21 23:36:08 +0900

    fix: sentry error tracking issue 해결

--- next.config.js
+++ next.config.js
@@ -11,6 +11,21 @@ const nextConfig = {
     tunnelRoute: '/monitoring-tunnel',
   },
 };
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  org: 'f-x-frontend',
+  project: 'f-x-frontend-qm',
+
+  silent: true, // Suppresses all logs
+
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
 
 module.exports = withSentryConfig(
   module.exports,
@@ -18,4 +33,4 @@ module.exports = withSentryConfig(
   { hideSourcemaps: true }
 );
 
-module.exports = nextConfig;
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
