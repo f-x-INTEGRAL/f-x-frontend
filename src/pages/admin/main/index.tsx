ed0e8db617e3b97e4e74e05829465a2f2594a40d
[1O16] <youiiyou1253@gmail.com>
2023-04-24 21:13:44 +0900

    feat: admin main 페이지 ui

--- src/pages/admin/main/index.tsx
+++ src/pages/admin/main/index.tsx
@@ -3,22 +3,63 @@ import styled from '@emotion/styled';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-export const DashboardWrapper = styled.div`
+const DashboardWrapper = styled.div`
   width: 1000px;
   height: 1000px;
   margin: auto 0;
 `;
 
-export const DashboardTable = styled.table``;
-export const DashboardTr = styled.tr``;
-export const DashboardTh = styled.th``;
+const DashboardTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  text-align: left;
+  margin-top: 50px;
+`;
+
+const DashboardThead = styled.thead`
+  background-color: #b8d7ff;
+`;
+
+const DashboardTheadTh = styled.th`
+  padding: 15px 0 15px 15px;
+  color: #abaaaa;
+  font-size: 19px;
+  font-weight: 400;
+`;
+
+const DashboardTbody = styled.tbody`
+  border-bottom: 2px solid #abaaaa;
+`;
+
+const DashboardTbodyTd = styled.td`
+  padding: 15px 0 15px 15px;
+  color: #abaaaa;
+  font-size: 18px;
+`;
+
+const ConfirmButton = styled.button`
+  margin-left: 10px;
+  width: 25px;
+  height: 25px;
+  font-size: 16px;
+  background: none;
+  border: 1px solid #62a1f1;
+  box-shadow: 1px 1px 5px #62a1f1;
+  border-radius: 30%;
+
+  &:hover {
+    background-color: #b8d7ff;
+    transition: 0.3s;
+  }
+
+  cursor: pointer;
+`;
 
 interface UserInfo {
   id: number;
   name: string;
   phoneNumber: string;
   quantity: string;
-  status: string;
 }
 
 const getUsers = async (): Promise<UserInfo[]> => {
@@ -35,6 +76,7 @@ const getUsers = async (): Promise<UserInfo[]> => {
 
 const adminMainPage = () => {
   const [users, setUsers] = useState<UserInfo[]>([]);
+  const [paymentConfirm, setPaymentConfirm] = useState<boolean>(false);
 
   useEffect(() => {
     const getDatas = async () => {
@@ -47,15 +89,44 @@ const adminMainPage = () => {
 
   return (
     <Layout>
-      {users.map((user) => (
+      <DashboardTable>
+        <DashboardThead>
+          <tr>
+            <DashboardTheadTh>ID</DashboardTheadTh>
+            <DashboardTheadTh>이름</DashboardTheadTh>
+            <DashboardTheadTh>전화번호</DashboardTheadTh>
+            <DashboardTheadTh>인원</DashboardTheadTh>
+            <DashboardTheadTh>입금확인</DashboardTheadTh>
+          </tr>
+        </DashboardThead>
+        <DashboardTbody>
+          <tr>
+            <DashboardTbodyTd>1</DashboardTbodyTd>
+            <DashboardTbodyTd>안기모</DashboardTbodyTd>
+            <DashboardTbodyTd>01012345678</DashboardTbodyTd>
+            <DashboardTbodyTd>4명</DashboardTbodyTd>
+            <DashboardTbodyTd>
+              {paymentConfirm ? '입금 완료' : '입금 대기'}
+              <ConfirmButton
+                onClick={() => setPaymentConfirm(!paymentConfirm)}
+                style={{
+                  color: paymentConfirm ? '#DF013A' : '#62a1f1',
+                }}
+              >
+                {paymentConfirm ? 'X' : 'O'}
+              </ConfirmButton>
+            </DashboardTbodyTd>
+          </tr>
+        </DashboardTbody>
+      </DashboardTable>
+      {/* {users.map((user) => (
         <li key={user.id}>
           <p>{user.id}</p>
           <p>{user.name}</p>
           <p>{user.phoneNumber}</p>
           <p>{user.quantity}명</p>
-          <p>{user.status}</p>
         </li>
-      ))}
+      ))} */}
     </Layout>
   );
 };
