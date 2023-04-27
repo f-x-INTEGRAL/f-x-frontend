7ea2f284d96922762b5d01a2395396c8e64d4a68
Gull 2365 <jych2365@gmail.com>
2023-04-28 01:30:31 +0900

    test: show table data test

--- src/pages/admin/main/index.tsx
+++ src/pages/admin/main/index.tsx
@@ -96,122 +96,49 @@ const getUsers = async (): Promise<UserInfo[]> => {
 const adminMainPage = () => {
   const [users, setUsers] = useState<UserInfo[]>([]);
 
-  const onClickStatus = () => {
-    const waitingUser = users.find((user) => user.status === 'WAITING');
-    const userId = users.find((user) => user.id);
-    if (userId !== null && userId !== undefined) {
-      const strUserId = userId.toString();
-      const intUserId = parseInt(strUserId);
-      if (waitingUser) {
-        axios.patch(
-          `https://fx.ggos3.xyz/admin/update/${intUserId}`,
-          {
-            status: 'CONFORMED',
-          },
-          {
-            withCredentials: true,
-          }
-        );
-      } else {
-        axios.patch(
-          `https://fx.ggos3.xyz/admin/update/${intUserId}`,
-          {
-            status: 'WAITING',
-          },
-          {
-            withCredentials: true,
-          }
-        );
-      }
-      const conformedUser = users.find((user) => user.status === 'CONFORMED');
-      if (waitingUser) {
-        axios.patch(
-          `https://fx.ggos3.xyz/admin/update/${waitingUser.id}`,
-          {
-            status: 'CONFORMED',
-          },
-          {
-            withCredentials: true,
-          }
-        );
-      } else if (conformedUser) {
-        axios.patch(
-          `https://fx.ggos3.xyz/admin/update/${conformedUser.id}`,
-          {
-            status: 'WAITING',
-          },
-          {
-            withCredentials: true,
-          }
-        );
-      }
-    }
-
-    const onClickDeleteUser = () => {
-      const userId = users.find((user) => user.id);
-
-      if (userId) {
-        axios.delete(`https://fx.ggos3.xyz/admin/delete/${userId}`, {
-          withCredentials: true,
-        });
-      }
-    };
-
-    useEffect(() => {
-      const getDatas = async () => {
-        const data = await getUsers();
-        setUsers(data);
-      };
-      getDatas();
-    }, []);
-
-    return (
-      <Layout>
-        <DashboardTable>
-          <DashboardThead>
-            <tr>
-              <DashboardTheadTh>ID</DashboardTheadTh>
-              <DashboardTheadTh>이름</DashboardTheadTh>
-              <DashboardTheadTh>전화번호</DashboardTheadTh>
-              <DashboardTheadTh>인원</DashboardTheadTh>
-              <DashboardTheadTh>입금확인</DashboardTheadTh>
+  return (
+    <Layout>
+      <DashboardTable>
+        <DashboardThead>
+          <tr>
+            <DashboardTheadTh>ID</DashboardTheadTh>
+            <DashboardTheadTh>이름</DashboardTheadTh>
+            <DashboardTheadTh>전화번호</DashboardTheadTh>
+            <DashboardTheadTh>인원</DashboardTheadTh>
+            <DashboardTheadTh>입금확인</DashboardTheadTh>
+          </tr>
+        </DashboardThead>
+        <DashboardTbody>
+          {users.map((user) => (
+            <tr
+              css={css`
+                background-color: white;
+                border-bottom: 1px solid #000;
+              `}
+            >
+              <DashboardTbodyTd>{user.id}</DashboardTbodyTd>
+              <DashboardTbodyTd>{user.name}</DashboardTbodyTd>
+              <DashboardTbodyTd>{user.phoneNumber}</DashboardTbodyTd>
+              <DashboardTbodyTd>{user.quantity}</DashboardTbodyTd>
+              <DashboardTbodyTd>
+                {user.status === 'CONFORMED' ? '입금 완료' : '입금 대기'}
+                <ConfirmButton
+                  style={{
+                    color: user.status === 'CONFORMED' ? '#62a1f1' : '#DF013A',
+                    border:
+                      user.status === 'CONFORMED'
+                        ? '1px solid #62a1f1'
+                        : '1px solid #DF013A',
+                  }}
+                >
+                  {user.status === 'CONFORMED' ? 'O' : 'X'}
+                </ConfirmButton>
+              </DashboardTbodyTd>
             </tr>
-          </DashboardThead>
-          <DashboardTbody>
-            {users.map((user) => (
-              <tr
-                css={css`
-                  background-color: white;
-                  border-bottom: 1px solid #000;
-                `}
-              >
-                <DashboardTbodyTd>{user.id}</DashboardTbodyTd>
-                <DashboardTbodyTd>{user.name}</DashboardTbodyTd>
-                <DashboardTbodyTd>{user.phoneNumber}</DashboardTbodyTd>
-                <DashboardTbodyTd>{user.quantity}</DashboardTbodyTd>
-                <DashboardTbodyTd>
-                  {user.status === 'CONFORMED' ? '입금 완료' : '입금 대기'}
-                  <ConfirmButton
-                    onClick={onClickStatus}
-                    style={{
-                      color:
-                        user.status === 'CONFORMED' ? '#62a1f1' : '#DF013A',
-                      border:
-                        user.status === 'CONFORMED'
-                          ? '1px solid #62a1f1'
-                          : '1px solid #DF013A',
-                    }}
-                  >
-                    {user.status === 'CONFORMED' ? 'O' : 'X'}
-                  </ConfirmButton>
-                  <DeleteButton onClick={onClickDeleteUser}>삭제</DeleteButton>
-                </DashboardTbodyTd>
-              </tr>
-            ))}
-          </DashboardTbody>
-        </DashboardTable>
-      </Layout>
-    );
-  };
+          ))}
+        </DashboardTbody>
+      </DashboardTable>
+    </Layout>
+  );
 };
 export default adminMainPage;
