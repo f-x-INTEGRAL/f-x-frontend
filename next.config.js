3d08e9b55917d1c4c82c63204fad0ca1917bb1dd
Gull 2365 <jych2365@gmail.com>
2023-04-28 01:14:33 +0900

    chore: source-map 제거

--- next.config.js
+++ next.config.js
@@ -1,44 +1,39 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 const { withSentryConfig } = require('@sentry/nextjs');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  sentry: {
-    tunnelRoute: '/monitoring-tunnel',
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/admin',
-        destination: '/admin/entry',
-      },
-    ];
-  },
-};
 const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
   org: 'f-x-frontend',
   project: 'f-x-frontend-qm',
 
   silent: true, // Suppresses all logs
-
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
-module.exports = withSentryConfig(
-  module.exports,
-  { silent: true },
-  { productionBrowserSourceMaps: false },
-  { hideSourcemaps: true }
-);
 
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+module.exports = withSentryConfig(
+  {
+    webpack: (config, options) => {
+      // 소스맵을 비활성화
+      config.devtool = false;
+
+      return config;
+    },
+
+    // Next.js 설정
+    reactStrictMode: true,
+    async rewrites() {
+      return [
+        {
+          source: '/admin',
+          destination: '/admin/entry',
+        },
+      ];
+    },
+    //vercel 연결
+    // Sentry 설정
+    sentry: {
+      disableServerWebpackPlugin: true,
+      disableClientWebpackPlugin: true,
+      tunnelRoute: '/monitoring-tunnel',
+      hideSourceMaps: true,
+    },
+  },
+  sentryWebpackPluginOptions
+);
