0cfef6bd7413ddb2c12a726b0ac6181174ac4fd6
Gull 2365 <jych2365@gmail.com>
2023-04-27 14:19:38 +0900

    feat: table 스타일 수정 완성

--- src/pages/admin/main/index.tsx
+++ src/pages/admin/main/index.tsx
@@ -121,7 +121,6 @@ const adminMainPage = () => {
                   {paymentConfirm ? 'X' : 'O'}
                 </ConfirmButton>
               </DashboardTbodyTd>
-              o
             </tr>
           ))}
         </DashboardTbody>
